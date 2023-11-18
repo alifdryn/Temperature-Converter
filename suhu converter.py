4e4b652da492563ea92f0590944ee94b49d8af11
Daryan Alifi <alif.daryan@gmail.com>
2023-11-18 21:35:25 +0700

    first commit

---
+++ suhu converter.py
@@ -0,0 +1,58 @@
+def kelvin_to_celsius(kelvin):
+    """
+    Mengonversi suhu dari Kelvin ke Celsius.
+    """
+    celsius = kelvin - 273.15
+    return celsius
+
+def celsius_to_kelvin(celsius):
+    """
+    Mengonversi suhu dari Celsius ke Kelvin.
+    """
+    kelvin = celsius + 273.15
+    return kelvin
+
+def temperature_converter(suhu, unit):
+    """
+    Mengonversi suhu dari Celsius atau Kelvin ke Fahrenheit.
+    """
+    if unit == 'C':
+        kelvin = celsius_to_kelvin(suhu)
+    elif unit == 'K':
+        kelvin = suhu
+    else:
+        raise ValueError("Unit harus 'C' atau 'K'")
+
+    fahrenheit = (kelvin - 273.15) * 9/5 + 32
+    return fahrenheit
+
+def fahrenheit_converter(suhu, output_unit):
+    """
+    Mengonversi suhu dari Fahrenheit ke Celsius atau Kelvin.
+    """
+    celsius = (suhu - 32) * 5/9
+    if output_unit == 'C':
+        return celsius
+    elif output_unit == 'K':
+        return celsius_to_kelvin(celsius)
+    else:
+        raise ValueError("Output unit harus 'C' atau 'K'")
+
+def main():
+    # Input suhu dan satuan dari pengguna
+    suhu_input = float(input("Masukkan suhu: "))
+    satuan_input = input("Masukkan satuan suhu ('C' untuk Celsius, 'K' untuk Kelvin): ").upper()
+
+    # Konversi ke Fahrenheit, Celsius, dan Kelvin
+    suhu_fahrenheit = temperature_converter(suhu_input, satuan_input)
+    suhu_celsius = fahrenheit_converter(suhu_fahrenheit, 'C')
+    suhu_kelvin = fahrenheit_converter(suhu_fahrenheit, 'K')
+
+    # Menampilkan hasil konversi
+    print(f"\nHasil Konversi:")
+    print(f"{suhu_input} {satuan_input} = {suhu_fahrenheit:.2f} Fahrenheit")
+    print(f"{suhu_input} {satuan_input} = {suhu_celsius:.2f} Celsius")
+    print(f"{suhu_input} {satuan_input} = {suhu_kelvin:.2f} Kelvin")
+
+if __name__ == "__main__":
+    main()
